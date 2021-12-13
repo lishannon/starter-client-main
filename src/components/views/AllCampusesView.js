12739365c9eecf25bc2e1c8b3d9a641386e776ac
lishannon <lishannon490@gmail.com>
2021-12-13 13:05:10 -0500

    Setting up the starter-client-main

---
+++ src/components/views/AllCampusesView.js
@@ -0,0 +1,31 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+const AllCampusesView = (props) => {
+  //const {campuses, deleteCampus} = props; 
+
+  if (!props.allCampuses.length) {
+    return <div>There are no campuses.</div>;
+  }
+
+  return (
+    <div>
+      {props.allCampuses.map((campus) => (
+        <div key={campus.id}>
+          <Link to={`/campus/${campus.id}`}>
+            <h1>{campus.name}</h1>
+          </Link>
+          <h3>{campus.address}</h3>
+          <p>{campus.description}</p>
+          {/* <button onClick={() => deleteCampus(campus.id)}>Delete</button> */}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+AllCampusesView.propTypes = {
+  allCampuses: PropTypes.array.isRequired,
+};
+
+export default AllCampusesView;
---
+++ src/components/views/StudentView.js
@@ -0,0 +1,13 @@
+const StudentView = (props) => {
+  const { student } = props;
+  return (
+    <div>
+      <h1>{student.firstname + " " + student.lastname}</h1>
+      <h2>{student.email} </h2>
+      <h3>{student.campus.name}</h3>
+    </div>
+  );
+
+};
+
+export default StudentView;
